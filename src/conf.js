2bbf8023c9f08b5c2343031bd6e6c4d390b5f446
AmetDev <ametsejdaliev@gmail.com>
2024-05-31 12:24:35 +0300

    fixed

--- src/conf.js
+++ src/conf.js
@@ -1,3 +1,4 @@
-// export const __VALUE__ = 'https://backend-for-online-school.onrender.com'
-export const __VALUE__ = 'http://localhost:4444'
-export const __VALUE_SEX__ = 'http://localhost:4425'
+export const __VALUE__ = 'https://backend-for-online-school.onrender.com'
+//export const __VALUE__ = 'http://localhost:4444'
+export const __VALUE_SEX__ =
+	'https://backend-for-online-school-1.onrender.com/:4425'
